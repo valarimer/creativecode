22a6f022a82d10ec44d91582225b197055032ad8
valarimer <54549414+valarimer@users.noreply.github.com>
2019-10-11 04:20:50 -0400

    Update and rename forthewall.js to halloweenscreen.js

---
+++ screensaver/halloweenscreen.js
@@ -0,0 +1,44 @@
+let bat;
+let cat;
+let ghost;
+let bckgrd;
+var changeDirection;
+var x = 0;
+var y = 100;
+var y2 = 500;
+var y3 = 900;
+var y4 = 1300;
+var y5 = 1400;
+
+//code used from forthewall.js//
+function preload(){
+  bckgrd= loadImage('Background.jpg');
+}
+
+function setup() {
+  createCanvas(1920, 1800);
+  bat = createImg('bat.gif');
+  bat2 = createImg('bat.gif');
+  cat = createImg('cat.gif');
+  ghost = createImg('ghost.gif');
+  ghost2 = createImg('ghost.gif');
+}
+
+function draw() {
+  background(bckgrd); 
+  bat.position(x,y);
+  ghost.position(x,y2);
+  bat2.position(x,y3);
+  ghost2.position(x,y4);
+  cat.position(x,y5);
+  
+  if(x>width){
+		changeDirection=true}
+	else if (x<=0){
+		changeDirection=false}
+	
+	if (x>=0 && changeDirection == false){
+		x=x+1}
+	else if(changeDirection == true){
+		x=x-1}
+}
